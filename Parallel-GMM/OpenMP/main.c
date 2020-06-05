fcbd16170f122208c8e946f2603d02b3c0340ad2
Dragon1573 <Legend_1949@126.com>
2020-06-05 11:30:09 +0800

    Realized GMM Clustering with C OpenMP
    In this project, I realized the algorithm with OpenMP v2.0. The serial algorithm is so difficult to parallelize, but you still can see a speed up when you time it. If you have any good realizations, FORK AND IMPROVE IT!

---
+++ Parallel-GMM/OpenMP/main.c
@@ -0,0 +1,41 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright @ 2020 Dragon1573
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include "clustering.h"
+
+int main(int argc, const char *argv[]) {
+    /* 语法检查 */
+    if (argc < 7) {
+        fprintf(
+            stderr,
+            "Usage: OpenMP.exe <threads> <input> <output> "
+            "<size> <dimensions> <clusters>\n"
+        );
+        return EXIT_FAILURE;
+    }
+
+    /* 解析参数 */
+    threads = strtoull(argv[1], NULL, 10);
+    dataSize = strtoull(argv[4], NULL, 10);
+    dimensions = strtoull(argv[5], NULL, 10);
+    clusters = strtoull(argv[6], NULL, 10);
+
+    /* 载入文件 */
+    loadFile(argv[2]);
+    /* KMeans聚类（用于初始化GMM） */
+    kMeans_clustering();
+    /* GMM聚类 */
+    gaussian_clustering();
+    /* 输出结果 */
+    saveFile(argv[3]);
+    /* 清理开辟的数组空间 */
+    cleanUp();
+
+    return EXIT_SUCCESS;
+}
